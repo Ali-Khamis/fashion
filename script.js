771a15ff924e8b6057c3a7f384a09164f5bee8ce
alikhamis <altramsia@gmail.com>
2020-10-21 13:33:03 +0200

    fixed some nav bugs

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const navTop = document.querySelectorAll(".secondarybar_item");
 const navbottom = document.querySelector(".primarybar");
 const hamMenu = document.querySelector(".secondarybar_hamMenu");
-const navBar = document.querySelector(".secondarybar");
-var prevScrollpos = window.pageYOffset;
-
+const secodaryNavBar = document.querySelector(".secondarybar");
+const navBar = document.querySelector("nav");
+const ul = document.querySelector("ul");
+let prevScrollpos = window.pageYOffset;
+let windowWidth = window.innerWidth;
 hamMenu.addEventListener("click", openHamMenu);
 function openHamMenu() {
   navTop.forEach((element) => {
@@ -11,17 +13,36 @@ function openHamMenu() {
   });
   navbottom.classList.toggle("active");
 }
-
-window.addEventListener("scroll", showingNavBar);
-function showingNavBar() {
-  var currentScrollPos = window.pageYOffset;
-  console.log(currentScrollPos);
-  if (currentScrollPos < prevScrollpos && currentScrollPos > 54) {
+if (windowWidth <= 768) {
+  navBar.style.position = "fixed";
+  navBar.style.top = "0";
+  secodaryNavBar.style.top = "0";
+}
+window.addEventListener("resize", () => {
+  windowWidth = window.innerWidth;
+  if (windowWidth <= 768) {
+    navBar.style.position = "fixed";
     navBar.style.top = "0";
-  } else if (currentScrollPos < 54) {
-    navBar.style.top = "54px";
+    secodaryNavBar.style.top = "0";
   } else {
-    navBar.style.top = "-152px";
+    navBar.style.position = "";
+    navBar.style.top = "";
+    secodaryNavBar.style.top = "";
+  }
+});
+
+window.addEventListener("scroll", showingsecodaryNavBar);
+function showingsecodaryNavBar() {
+  let currentScrollPos = window.pageYOffset;
+  if (
+    (currentScrollPos < prevScrollpos && currentScrollPos > 54) ||
+    (currentScrollPos < prevScrollpos && windowWidth <= 768)
+  ) {
+    secodaryNavBar.style.top = "0";
+  } else if (currentScrollPos < 54) {
+    secodaryNavBar.style.top = "54px";
+  } else {
+    secodaryNavBar.style.top = "-100%";
   }
   prevScrollpos = currentScrollPos;
 }
